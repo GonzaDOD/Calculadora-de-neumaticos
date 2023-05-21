3c5f1c1d759f2ca4cb068bbee34990ffe9450aef
GonzaDOD <34110616+GonzaDOD@users.noreply.github.com>
2023-05-21 16:44:45 -0300

    Actualización main.py
    Se extendió la lista de valores y se cambió la forma de tomar los datos del neumático para evitar errores, introduciendo los valores separados por espacios.

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
 # La línea superior habilita las tildes en nuestro código
+
 def neumatico():
-    n = input("Ingresá los datos del neumático sin espacios(220/40R1880H): ")  
-    # Obtenemos los datos del neumático para realizar los cálculos y recopilar información
+    n = input("Ingresá los datos del neumático con espacios(255/55 R 20 110 V): ")
     if n == "":
-        # Si no se introduce ningún dato
-        return "220/40R1880H" # Devuelve el ejemplo
-neumatico = neumatico() # Almacenamos el valor que se obtenga, dentro de una variable
+        return "255/55 R 20 110 V"
+    else:
+        return n
 
-neumatico_list = list(neumatico)  # Lo convertimos en lista para poder acceder a sus dígitos
 
-if len(neumatico) != 12:  # Verificamos si tiene 12 dígitos y no tiene espacios
+neumatico = neumatico()
+# Obtenemos los datos del neumático para realizar los cálculos y recopilar información
+neumatico_list = neumatico.split(" ")  # Lo convertimos en una lista separada para poder acceder a sus dígitos.
+
+if len(neumatico_list) != 5:  # Verificamos si la lista tiene los 5 indices,
     raise Exception(
-        "Los datos ingresados no son correctos, por favor, intente nuevamente")  
+        "Los datos ingresados no son correctos, por favor, intente nuevamente")
     # Si la condición se cumple, se obtiene una excepción.
 
 else:  # Si, por lo contrario, los datos introducidos son correctos, se procede a realizar los cálculos
-    ancho_pisada = neumatico_list[0] + neumatico_list[1] + neumatico_list[
-        2]  # Obtenemos los 3 primeros digitos que pertenecen al ancho de pisada
-    porcentaje_flanco = neumatico_list[4] + neumatico_list[
-        5]  # Obtenemos los 2 digitos que siguen después de la barra diagonal (/)
-    construccion = neumatico_list[6]  # Obtenemos el codigo de construcción (R/D)
-    diametro_llanta_p = neumatico_list[7] + neumatico_list[
-        8]  # Obtenemos el diametro de la llanta como los siguientes 2 dígitos
-    codigo_carga = neumatico_list[9] + neumatico_list[10]  # Obtenemos el código de la capacidad de carga
-    codigo_velocidad = neumatico_list[11]  # Obtenemos el código de la capacidad de carga
+    dato1 = neumatico_list[0].split("/")
+    ancho_pisada = dato1[0]  # Obtenemos los primeros digitos que pertenecen al ancho de pisada
+    porcentaje_flanco = dato1[1]  # Obtenemos los 2 digitos que siguen después de la barra diagonal (/)
+    construccion = neumatico_list[1]  # Obtenemos el codigo de construcción (R/D)
+    diametro_llanta_p = neumatico_list[2]  # Obtenemos el diametro de la llanta
+    codigo_carga = neumatico_list[3]  # Obtenemos el código de la capacidad de carga
+    codigo_velocidad = neumatico_list[4]  # Obtenemos el código de la capacidad de carga
 
     construccion_dict = {  # Obtenemos los dos valores posibles de construccion, en un diccionario
         "R": "Radial",
@@ -32,6 +33,7 @@ else:  # Si, por lo contrario, los datos introducidos son correctos, se procede 
     }
 
     diametro_llanta_mm = int(int(diametro_llanta_p) * 25.4)
+
     velocidad_max = {  # Obtenemos los valores de la tabla de velocidad, en un diccionario
         "L": "120",
         "M": "130",
@@ -47,7 +49,7 @@ else:  # Si, por lo contrario, los datos introducidos son correctos, se procede 
         "Y": "300",
         "ZR": "Mayor a 240",
     }
-    capacidad_carga = {  # Obtenemos los valores de la tabla de capacidad, en un diccionario
+    capacidad_carga = {
         "70": "335",
         "71": "345",
         "72": "355",
@@ -69,7 +71,28 @@ else:  # Si, por lo contrario, los datos introducidos son correctos, se procede 
         "88": "560",
         "89": "580",
         "90": "600",
+        "91": "615",
+        "92": "630",
+        "93": "660",
+        "94": "670",
+        "95": "690",
+        "96": "710",
+        "97": "730",
+        "98": "750",
+        "99": "775",
+        "100": "800",
+        "101": "825",
+        "102": "850",
+        "103": "875",
+        "104": "900",
+        "105": "925",
+        "106": "950",
+        "107": "975",
+        "108": "1000",
+        "109": "1030",
+        "110": "1060"
     }
+
     print(f"""
     Ancho de pisada: {ancho_pisada}mm
     Medida del flanco: {int(int(ancho_pisada) * int(porcentaje_flanco) / 100)}mm
@@ -78,6 +101,5 @@ else:  # Si, por lo contrario, los datos introducidos son correctos, se procede 
     Capacidad de Carga: {capacidad_carga[codigo_carga]}Kg
     Velocidad Máxima: {velocidad_max[codigo_velocidad.upper()]}Km/h
     """)
-    input()
     # Mostramos en pantalla la información recopilada haciendo los cálculos necesarios
     # incluyendo la unidad de los valores y añadiendo el ".upper" para permitir minúsculas.
